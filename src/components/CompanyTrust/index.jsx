e09d5bcb0fec09d709dd4d650d665f4ab903ffc0
EdithOrt <eoc.diseno@gmail.com>
2021-02-19 09:43:34 -0600

    updates home in footer and browser section

--- src/components/CompanyTrust/index.jsx
+++ src/components/CompanyTrust/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import image1 from '../../assets/images/image1.png'
+import image2 from '../../assets/images/image2.png'
+import image3 from '../../assets/images/image3.png'
+import image4 from '../../assets/images/image4.png'
+import image5 from '../../assets/images/image5.png'
 
 const CompanyTrust = () => {
     return ( 
-        <div className='company-trust-container'>
+        <div className='company-trust-container' style={{marginTop:'50px'}}>
             <div>
-                <p>Ellos ya confían en nosotros</p>
+                <p style={{fontSize:'25px', marginTop:'50px'}}>Ellos ya confían en nosotros</p>
             </div>
-            <div>
-                <img alt='img1'/>
-                <img alt='img2'/>
-                <img alt='img3'/>
-                <img alt='img4'/>
-                <img alt='img5'/>
+            <div className='images-logos-container'>
+                <img alt='img1' style={{marginRight:'20px', marginTop:'20px'}} src={image1}/>
+                <img alt='img2' style={{marginRight:'20px', marginTop:'20px'}} src={image2}/>
+                <img alt='img3' style={{marginRight:'20px', marginTop:'20px'}} src={image3}/>
+                <img alt='img4' style={{marginRight:'20px', marginTop:'20px'}} src={image4}/>
+                <img alt='img5' style={{marginRight:'20px', marginTop:'20px'}} src={image5}/>
             </div>
         </div>
      );
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import logo from '../../assets/images/logo.png'
+
+import {FacebookFilled, InstagramFilled, LinkedinFilled, TwitterCircleFilled} from '@ant-design/icons'
 
 const Footer = () => {
     return ( 
         <div className='footer'>
-            <p>Este es el footer</p>
+            <div>
+                <img src={logo} alt='logo'/>
+            </div>
+            <div style={{marginBottom: '45px'}}>
+                <FacebookFilled style={{fontSize: '30px', color:'#ffffff', marginRight:'10px'}} />
+                <InstagramFilled style={{fontSize: '30px', color:'#ffffff', marginRight:'10px'}} />
+                <LinkedinFilled style={{fontSize: '30px', color:'#ffffff', marginRight:'10px'}} />
+                <TwitterCircleFilled style={{fontSize: '30px', color:'#ffffff', marginRight:'10px'}} />
+            </div>
         </div>
      );
 }
